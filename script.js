ced284d9fef9166ba11d3422b4c06c703887e8f5
Kao Bui <kaonghia035@gmail.com>
2024-12-08 23:36:05 +0100

    Add Entrance Animation

--- script.js
+++ script.js
@@ -14,91 +14,43 @@ function raf(time) {
 
 requestAnimationFrame(raf); // Start the loop
 
-function togglemenu() {
-  var x = document.getElementById("nav");
-  if (x.className === "nav") {
-    x.className += " nav--open";
-  } else {
-    x.className = "nav";
-  }
-  var element = document.getElementById("menu-toggle");
-  element.classList.toggle("menu-toggle--open");
-}
-
 window.onload = function () {
   setTimeout(function () {
     document.getElementById("fadein").remove();
   }, 1000);
 };
 
-function handleButtonClick(event) {
-  const button = event.target;
-  const card = button.parentElement.parentElement;
-  const cards = document.querySelectorAll('.accordeon-card');
-  const buttons = document.querySelectorAll('.accordeon-content a')
-
-  // Remove the 'active' class from all cards
-  cards.forEach((c) => c.classList.remove('active'));
-
-
-  // Add the 'active' class to the clicked card
-  card.classList.add('active');
-  setTimeout(() => { button.innerHTML = 'See Details'; },
-    500);
-
-  buttons.forEach((b) => {
-    if (b !== button && b.innerHTML === 'See Details') {
-      b.innerHTML = '+'
-    }
-  });
-}
-
-
-const cards = document.querySelectorAll('.features-bloc');
-
-cards.forEach((card) => {
-  card.addEventListener('click', () => {
-    cards.forEach((otherCard) => {
-      otherCard.classList.remove('active');
-    });
-
-    card.classList.add('active');
+gsap.utils.toArray(".slide-up-out").forEach((text, i) => {
+  gsap.to(text, {
+    scrollTrigger: {
+      trigger: text,
+      start: "top 10%",
+      end : "top top",
+      scrub: 1,
+      markers: true,
+    },
+    y: "-10vh",
+    opacity: 0,
   });
 });
 
-var photoContainer = document.querySelector('.caroussel .img-container');
-var photoWidth = document.querySelector('.caroussel .img-container img').offsetWidth + 10;
+const splitTextElements = document.querySelectorAll(".split-text");
 
-function scrollToPhoto(index) {
-  var scrollAmount = photoWidth * index;
-  photoContainer.style.transform = 'translateX(-' + scrollAmount + 'px)';
-}
+splitTextElements.forEach((textElement) => {
+  const text = textElement.textContent;
 
-cards.forEach(function (button, index) {
-  button.addEventListener('click', function () {
-    scrollToPhoto(index);
-  });
+  textElement.innerHTML = text
+    .split("")
+    .map((char) => `<span>${char === " " ? "&nbsp;" : char}</span>`)
+    .join("");
 });
 
-
-const questions = document.querySelectorAll('.question-bloc')
-const questionContents = document.querySelectorAll('.question-content .answer')
-
-questions.forEach((question) => {
-  question.addEventListener('click', () => {
-    const questionAnswer = question.querySelector('.answer');
-    if (question.classList.contains('active')) {
-      question.classList.remove('active');
-      questionAnswer.style.height = '0px';
-    } else {
-      questions.forEach((otherQuestions) => {
-        otherQuestions.classList.remove('active');
-      });
-      question.classList.add('active');
-      questionContents.forEach((content) => {
-        content.style.height = '0px';
-      });
-      questionAnswer.style.height = questionAnswer.scrollHeight + 'px';
-    }
-  })
-})
+let tl = gsap.timeline();
+tl.from(".split-text span", {
+  duration: 1,
+  y: 150,
+  autoAlpha: 0,
+  ease: Power3.out,
+  stagger: 0.1,
+  delay: 0.5,
+});
